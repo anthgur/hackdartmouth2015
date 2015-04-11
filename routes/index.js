37eb991bdb8a26f0abcc4c69ce7d0237dbe3d749
Maxwell Renke <maxrenke@gmail.com>
2015-04-11 19:42:15 -0400

    router.post

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,8 @@
 var express = require('express');
 var router = express.Router();
  
-router.get('/webapp', function(req, res) {
+router.post('/webapp', function(req, res) {
     if (req.method == 'POST') {
-        /*var body = '';
-        req.on('data', function (data) {
-            body += data;
-            console.log("Partial body: " + body);
-        });
-        req.on('end', function () {
-            console.log("Body: " + body);
-        });
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('post received');*/
 		res.json({ message: "POST" });
     }
     else
