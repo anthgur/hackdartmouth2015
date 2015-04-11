a085c5ab5700445e4959e84055726dc8d039204f
Maxwell Renke <maxrenke@gmail.com>
2015-04-11 17:02:08 -0400

    added route and web app html form

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,18 @@ router.get('/', function(req, res) {
 });
 
 router.get('/webapp', function(req, res) {
-  res.json({ message: "webapp" });
+  fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
 });
+
+
 
 module.exports = router;
 
