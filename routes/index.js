a008cd0603cde72add3226fe1146e512826b471d
Maxwell Renke <maxrenke@gmail.com>
2015-04-11 16:45:45 -0400

    added webapp response

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,9 @@ router.get('/', function(req, res) {
   res.json({ message: "Oh hai" });
 });
 
+router.get('/webapp', function(req, res) {
+  res.json({ message: "webapp" });
+});
+
 module.exports = router;
 
