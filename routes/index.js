8b34b39b5cca870fbd85b1d0e41024d3aca41e32
Maxwell Renke <maxrenke@gmail.com>
2015-04-11 19:27:46 -0400

    display post

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,29 @@ var express = require('express');
 var router = express.Router();
  
 router.get('/', function(req, res) {
-	if(req.method == "POST" ) {
-		res.json({ message: "Oh hai" });
-	}
-	else
-		res.json({ message: "Goodbye" });
+	console.dir(req.param);
+
+    if (req.method == 'POST') {
+        console.log("POST");
+        var body = '';
+        req.on('data', function (data) {
+            body += data;
+            console.log("Partial body: " + body);
+        });
+        req.on('end', function () {
+            console.log("Body: " + body);
+        });
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('post received');
+    }
+    else
+    {
+        console.log("GET");
+        //var html = '<html><body><form method="post" action="http://localhost:3000">Name: <input type="text" name="name" /><input type="submit" value="Submit" /></form></body>';
+        var html = fs.readFileSync('index.html');
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(html);
+    }
 });
 
 module.exports = router;
