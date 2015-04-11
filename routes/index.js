26b339f0a14ad36d6944ccdec540a0448df88cfa
Maxwell Renke <maxrenke@gmail.com>
2015-04-11 19:56:07 -0400

    bodyParser

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
- 
+
+app.use(express.bodyParser());
+
 router.post('/webapp', function(req, res) {
     if (req.method == 'POST') {
-		res.json({ message: "POST" });
     }
     else
     {
