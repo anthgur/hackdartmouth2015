acc2054eb8aeef4955daa93430d31686e24ea497
Maxwell Renke <maxrenke@gmail.com>
2015-04-11 17:17:38 -0400

    reverting

--- routes/index.js
+++ routes/index.js
@@ -5,19 +5,5 @@ router.get('/', function(req, res) {
   res.json({ message: "Oh hai" });
 });
 
-router.get('/webapp', function(req, res) {
-  fs.readFile('./webapp.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(8000);
-});
-
-
-
 module.exports = router;
 
