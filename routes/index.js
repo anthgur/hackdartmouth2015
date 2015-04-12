85bd7f16b353db7841bc226b596fb3e63d41f1b9
Maxwell Renke <maxrenke@gmail.com>
2015-04-11 20:24:52 -0400

    just webapp

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/desktop', function(req, res) {
-    if (req.method == 'POST') {
-		res.json({ message: "POST desktop" });
-	}
-	else{
-		res.json({ message: "GET desktop" });
-	}
-	
-});
-
 router.post('/webapp', function(req, res) {
     if (req.method == 'POST') {
-		res.json({ message: "POST webapp" });
+		res.json({ message: "POST" });
     }
     else
     {
-        res.json({ message: "GET webapp" });
+        res.json({ message: "GET" });
     }
 });
 
